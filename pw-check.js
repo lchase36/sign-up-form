8393d6b9d996339108148a336c2bf452a8d11f97
lchase36 <chaselesh36@gmail.com>
2023-03-22 00:25:41 -0700

    Further styled the page to ensure responsive desktop format. Created form patterns and script to check if password and password confirmation match.

---
+++ pw-check.js
@@ -0,0 +1,19 @@
+const passElem = document.querySelector("#password");
+const confPassElem = document.querySelector("#confirm-password");
+const passError = document.querySelector("#password + span.error");
+const confPassError = document.querySelector("#confirm-password + span.error");
+
+confPassElem.addEventListener("input", checkPass);
+passElem.addEventListener("input", checkPass);
+function checkPass() {
+	let msg = "&nbsp;";
+	if (passElem.value != confPassElem.value && confPassElem.value != "") {
+		msg = "* Passwords do not match";
+		passElem.className = "input-error";
+		confPassElem.className = "input-error";
+	} else {
+		passElem.className = "";
+		confPassElem.className = "";
+	}
+	confPassError.textContent = msg;
+}
